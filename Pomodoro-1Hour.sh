a681e7a43a907dca7968dcf61505b991fc92a699
Aaron Ashley-Gaddis <aaronashley466@gmail.com>
2026-03-07 08:05:08 -0700

    Create Pomodoro-1Hour.sh

---
+++ Pomodoro-1Hour.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting 1 hour Pomodoro..."
+sleep 3600 # 1 hour
+notify-send "Break time! Take 15 minutes, trust me you need it."
+sleep 900 # 15 minute break
+notify-send "Back to work, keep at it and try harder. Start the next cycle, remember to take breaks!"
